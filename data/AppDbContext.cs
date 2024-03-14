91df2b1d10f62863b32cee283dcaa01e3cebd797
Idrissa <idrisabkar2018@gmail.com>
2024-03-14 15:57:51 +0300

    Add password hashing for Admin and Author entities in SaveChanges() method

--- data/AppDbContext.cs
+++ data/AppDbContext.cs
@@ -13,6 +13,37 @@ namespace blogApi.data
         {
             optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=finshark_db;Username=postgres;Password=idris2014");
         }
+        public override int SaveChanges()
+        {
+            // Admin password hashing
+            foreach (var entry in ChangeTracker.Entries<Admin>())
+            {
+                if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
+                {
+                    var user = entry.Entity;
+                    if (!string.IsNullOrEmpty(user.PasswordHash) && user.PasswordHash.Length < 50)
+                    {
+                        user.PasswordHash = PasswordHasher.HashPassword(user.PasswordHash);
+                    }
+                }
+            }
+
+            // Author password hashing
+            foreach (var entry in ChangeTracker.Entries<Author>())
+            {
+                if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
+                {
+                    var user = entry.Entity;
+                    if (!string.IsNullOrEmpty(user.PasswordHash) && user.PasswordHash.Length < 50)
+                    {
+                        user.PasswordHash = PasswordHasher.HashPassword(user.PasswordHash);
+                    }
+                }
+            }
+
+
+            return base.SaveChanges();
+        }
         public DbSet<Admin> Admins { set; get; }
         public DbSet<Article> Articles { set; get; }
         public DbSet<Author> Authors { set; get; }
---
+++ utils/PasswordHasher.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class PasswordHasher
+{
+    public static string HashPassword(string password)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(hashedBytes);
+        }
+    }
+}
