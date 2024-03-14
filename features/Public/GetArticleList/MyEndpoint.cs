6fa00ee112d2a785abb8874f3ca025fc0f1c30cc
Idrissa <idrisabkar2018@gmail.com>
2024-03-14 16:53:48 +0300

    Update code formatting and fix bug in function XYZ

---
+++ features/Public/GetArticleList/MyEndpoint.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FastEndpoints;
+
+
+namespace Public.GetArticleById
+{
+    public class MyEndpoint : Endpoint<Request, Response>
+    {
+        public override void Configure()
+        {
+            Post("/admin/login");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(Request req, CancellationToken ct)
+        {
+            await SendAsync(new Response()
+            {
+                Message = $"Welcome {req.FirstName} {req.LastName}"
+            });
+        }
+    }
+}
